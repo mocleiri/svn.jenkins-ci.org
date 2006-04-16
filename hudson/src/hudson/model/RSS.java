6923e0f32635e466393074a0cb75b198ca6cf7d6
kohsuke <kohsuke@jenkins-ci.org>
2006-04-16 04:15:11 +0000

    wrote the code to parse trackback ping request, although I don't know if we are going to use it any time soon.
    git-svn-id: https://svn.jenkins-ci.org/trunk@322 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/src/hudson/model/RSS.java
+++ hudson/src/hudson/model/RSS.java
@@ -6,6 +6,7 @@ import org.kohsuke.stapler.StaplerResponse;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Comparator;
@@ -20,6 +21,27 @@ import java.util.TreeSet;
  * @author Kohsuke Kawaguchi
  */
 final class RSS {
+
+    /**
+     * Parses trackback ping.
+     */
+    public static void doTrackback( Object it, StaplerRequest req, StaplerResponse rsp ) throws IOException, ServletException {
+        String title = req.getParameter("title");
+        String url = req.getParameter("url");
+        String excerpt = req.getParameter("excerpt");
+        String blog_name = req.getParameter("blog_name");
+
+        rsp.setStatus(HttpServletResponse.SC_OK);
+        rsp.setContentType("application/xml; charset=UTF-8");
+        PrintWriter pw = rsp.getWriter();
+        pw.println("<response>");
+        pw.println("<error>"+(url!=null?0:1)+"</error>");
+        if(url==null) {
+            pw.println("<message>url must be specified</message>");
+        }
+        pw.println("</response>");
+        pw.close();
+    }
     /**
      * RSS feed for all runs.
      */
