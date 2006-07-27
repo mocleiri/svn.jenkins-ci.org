9ea36e38d9d4f1ebc1d5883d1fca22e376650a09
kohsuke <kohsuke@jenkins-ci.org>
2006-07-27 04:06:16 +0000

    more natural to put SCM trigger upfront.
    git-svn-id: https://svn.jenkins-ci.org/trunk@489 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/src/hudson/triggers/Triggers.java
+++ hudson/src/hudson/triggers/Triggers.java
@@ -10,7 +10,7 @@ public class Triggers {
      * List of all installed {@link Trigger}s.
      */
     public static final Descriptor<Trigger>[] TRIGGERS = Descriptor.toArray(
-        TimerTrigger.DESCRIPTOR,
-        SCMTrigger.DESCRIPTOR
+        SCMTrigger.DESCRIPTOR,
+        TimerTrigger.DESCRIPTOR
     );
 }
