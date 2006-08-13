83612441857e23d75cc75b59792a781d1d696fc9
kohsuke <kohsuke@jenkins-ci.org>
2006-08-13 05:50:02 +0000

    added javadoc target.
    git-svn-id: https://svn.jenkins-ci.org/trunk@638 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ hudson/publish-javadoc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+#
+# publish Hudson javadoc and deploy that into the java.net CVS repository
+# 
+
+ant javadoc
+
+cd ../www/javadoc
+cvs update -Pd
+
+cp -R ../../hudson/build/javadoc/* .
+
+# ignore everything under CVS, then
+# ignore all files that are already in CVS, then
+# add the rest of the files
+find . -name CVS -prune -o -exec bash in-cvs.sh {} \; -o \( -print -a -exec cvs add {} \+ \)
+
+# sometimes the first commit fails
+# cvs commit -m "commit 1 " || cvs commit -m "commit 2"
