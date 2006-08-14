97ced753abfe38821fd901209eb74db249601fd8
kohsuke <kohsuke@jenkins-ci.org>
2006-08-14 04:49:38 +0000

    moved plugint to its own top-level directory (so that Hudson main build CI won't pick up changes in plugins
    git-svn-id: https://svn.jenkins-ci.org/trunk@650 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ plugins/jwsdp-sqe/src/hudson/plugins/jwsdp_sqe/Test.java
@@ -0,0 +1,18 @@
+package hudson.plugins.jwsdp_sqe;
+
+import hudson.model.Build;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * A {@link Test} is a set of {@link TestCase}s.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class Test extends TestCollection<Test,TestCase> {
+    public String getChildTitle() {
+        return "Test Case";
+    }
+}
