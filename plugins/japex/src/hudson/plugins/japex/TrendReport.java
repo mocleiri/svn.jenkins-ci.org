7b2dbd1cc4fca0f8ec559a8d1e66f4485a5914a7
kohsuke <kohsuke@jenkins-ci.org>
2006-09-01 01:30:13 +0000

    fixed a bug where test cases with '/' in their names break the page rendering.
    git-svn-id: https://svn.jenkins-ci.org/trunk@740 71c3de6d-444a-0410-be80-ed276b4c234a

--- plugins/japex/src/hudson/plugins/japex/TrendReport.java
+++ plugins/japex/src/hudson/plugins/japex/TrendReport.java
@@ -1,14 +1,15 @@
 package hudson.plugins.japex;
 
 import com.sun.japex.report.MeanMode;
-import hudson.util.ChartUtil;
-import hudson.model.Project;
 import hudson.model.ModelObject;
+import hudson.model.Project;
+import hudson.util.ChartUtil;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represents a trend report.
@@ -21,11 +22,19 @@ public class TrendReport implements ModelObject {
 
     private final String configName;
     private final Project project;
+    /**
+     * Conversion from escaped test case names to original test case names,
+     * since test case names may contain URL-unsafe characters.
+     */
+    private final Map<String,String> testCaseNames = new HashMap<String, String>();
 
     TrendReport(Project project, String configName, HudsonChartGenerator chartGen) {
         this.project = project;
         this.configName = configName;
         this.chartGen = chartGen;
+        for (String name : chartGen.getTestNames()) {
+            testCaseNames.put( name.replace('/','_'), name );
+        }
     }
 
     /**
@@ -36,10 +45,10 @@ public class TrendReport implements ModelObject {
     }
 
     /**
-     * Gets all the test case names.
+     * Gets all the test case names from safe names to unsafe names.
      */
-    public Collection<String> getTestCaseNames() throws IOException {
-        return chartGen.getTestNames();
+    public Map<String,String> getTestCaseNames() throws IOException {
+        return testCaseNames;
     }
 
     public Project getProject() {
@@ -55,8 +64,8 @@ public class TrendReport implements ModelObject {
     /**
      * Gets to the object that represents individual test caase result.
      */
-    public TestCaseGraph getTestCaseGraph(String name) {
-        return new TestCaseGraph(this,name);
+    public TestCaseGraph getTestCaseGraph(String safeName) {
+        return new TestCaseGraph(this,testCaseNames.get(safeName));
     }
 
     public void doArithmeticMeanGraph(StaplerRequest req, StaplerResponse rsp) throws IOException {
