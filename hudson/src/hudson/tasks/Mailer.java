042377f37fa4036312a20fe634c66930297d6448
jglick <jglick@jenkins-ci.org>
2006-09-20 19:35:54 +0000

    Hyperlinking filenames in log section of build failure mail to published artifacts, or failing that, to the workspace.
    git-svn-id: https://svn.jenkins-ci.org/trunk@801 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/src/hudson/tasks/Mailer.java
+++ hudson/src/hudson/tasks/Mailer.java
@@ -8,14 +8,7 @@ import hudson.model.BuildListener;
 import hudson.model.Descriptor;
 import hudson.model.Project;
 import hudson.model.Result;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -24,7 +17,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.StringTokenizer;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.tools.ant.types.selectors.SelectorUtils;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
@@ -33,6 +37,8 @@ import org.kohsuke.stapler.StaplerRequest;
  * @author Kohsuke Kawaguchi
  */
 public class Mailer extends Publisher {
+    
+    private static final Logger LOGGER = Logger.getLogger(Mailer.class.getName());
 
     private static final int MAX_LOG_LINES = 250;
 
@@ -154,16 +160,53 @@ public class Mailer extends Publisher {
         try {
             String log = build.getLog();
             String[] lines = log.split("\n");
-            if (lines.length <= MAX_LOG_LINES) {
-                buf.append(log);
-            } else {
+            int start = 0;
+            if (lines.length > MAX_LOG_LINES) {
                 // Avoid sending enormous logs over email.
                 // Interested users can always look at the log on the web server.
                 buf.append("[...truncated " + (lines.length - MAX_LOG_LINES) + " lines...]\n");
-                for (int i = lines.length - MAX_LOG_LINES; i < lines.length; i++) {
-                    buf.append(lines[i]);
-                    buf.append('\n');
+                start = lines.length - MAX_LOG_LINES;
+            }
+            String workspaceUrl = null, artifactUrl = null;
+            Pattern wsPattern = null;
+            String baseUrl = DESCRIPTOR.getUrl();
+            if (baseUrl != null) {
+                // Hyperlink local file paths to the repository workspace or build artifacts.
+                // Note that it is possible for a failure mail to refer to a file using a workspace
+                // URL which has already been corrected in a subsequent build. To fix, archive.
+                workspaceUrl = baseUrl + Util.encode(build.getProject().getUrl()) + "ws/";
+                artifactUrl = baseUrl + Util.encode(build.getUrl()) + "artifact/";
+                File workspaceDir = build.getProject().getWorkspace().getLocal();
+                // Match either file or URL patterns, i.e. either
+                // c:\hudson\workdir\jobs\foo\workspace\src\Foo.java
+                // file:/c:/hudson/workdir/jobs/foo/workspace/src/Foo.java
+                // will be mapped to one of:
+                // http://host/hudson/job/foo/ws/src/Foo.java
+                // http://host/hudson/job/foo/123/artifact/src/Foo.java
+                // Careful with path separator between $1 and $2:
+                // workspaceDir will not normally end with one;
+                // workspaceDir.toURI() will end with '/' if and only if workspaceDir.exists() at time of call
+                wsPattern = Pattern.compile("(\\Q" + workspaceDir + "\\E|\\Q" + workspaceDir.toURI() + "\\E)[/\\\\]?([^:#\\s]*)");
+            }
+            for (int i = start; i < lines.length; i++) {
+                String line = lines[i];
+                if (wsPattern != null) {
+                    // Perl: $line =~ s{$rx}{$path = $2; $path =~ s!\\\\!/!g; $workspaceUrl . $path}eg;
+                    Matcher m = wsPattern.matcher(line);
+                    int pos = 0;
+                    while (m.find(pos)) {
+                        String path = m.group(2).replace(File.separatorChar, '/');
+                        String linkUrl = DESCRIPTOR.artifactMatches(path, build) ? artifactUrl : workspaceUrl;
+                        // Append ' ' to make sure mail readers do not interpret following ':' as part of URL:
+                        String prefix = line.substring(0, m.start()) + linkUrl + Util.encode(path) + ' ';
+                        pos = prefix.length();
+                        line = prefix + line.substring(m.end());
+                        // XXX better style to reuse Matcher and fix offsets, but more work
+                        m = wsPattern.matcher(line);
+                    }
                 }
+                buf.append(line);
+                buf.append('\n');
             }
         } catch (IOException e) {
             // somehow failed to read the contents of the log
@@ -192,7 +235,7 @@ public class Mailer extends Publisher {
         return msg;
     }
 
-    public hudson.model.Descriptor<Publisher> getDescriptor() {
+    public Descriptor<Publisher> getDescriptor() {
         return DESCRIPTOR;
     }
 
@@ -257,6 +300,28 @@ public class Mailer extends Publisher {
 
         public String getUrl() {
             return (String)getProperties().get("mail.hudson.url");
+        }
+
+        /** Check whether a path (/-separated) will be archived. */
+        public boolean artifactMatches(String path, Build build) {
+            ArtifactArchiver aa = (ArtifactArchiver) build.getProject().getPublishers().get(ArtifactArchiver.DESCRIPTOR);
+            if (aa == null) {
+                LOGGER.finer("No ArtifactArchiver found");
+                return false;
+            }
+            String artifacts = aa.getArtifacts();
+            for (String include : artifacts.split("[, ]+")) {
+                String pattern = include.replace(File.separatorChar, '/');
+                if (pattern.endsWith("/")) {
+                    pattern += "**";
+                }
+                if (SelectorUtils.matchPath(pattern, path)) {
+                    LOGGER.log(Level.FINER, "DescriptorImpl.artifactMatches true for {0} against {1}", new Object[] {path, pattern});
+                    return true;
+                }
+            }
+            LOGGER.log(Level.FINER, "DescriptorImpl.artifactMatches for {0} matched none of {1}", new Object[] {path, artifacts});
+            return false;
         }
 
         public Publisher newInstance(StaplerRequest req) {
