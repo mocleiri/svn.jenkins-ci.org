6a0d8b966ad7f36daa84529f5468f603fc838349
kohsuke <kohsuke@jenkins-ci.org>
2006-10-21 15:11:23 +0000

    added usage.
    git-svn-id: https://svn.jenkins-ci.org/trunk@890 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/resources/scripts/sortable.js
+++ hudson/resources/scripts/sortable.js
@@ -26,6 +26,22 @@ AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 OR OTHER DEALINGS IN THE SOFTWARE.
 */
+/*
+Usage
+=====
+
+Add the "sortable" CSS class to a table to make it sortable.
+The first column must be always table header, and the rest must be table data.
+(the script seems to support rows to be fixed at the bottom, but haven't figured out how to use it.)
+
+If the table data is sorted to begin with, you can add 'initialSortDir="up|down"' to the
+corresponding column in the header row to display the direction icon from the beginning.
+This is recommended to provide a visual cue that the table can be sorted.
+
+The script guesses the table data, and try to use the right sorting algorithm.
+But you can override this behavior by having 'data="..."' attribute on each row,
+in which case the sort will be done on that field.
+*/
 addEvent(window, "load", sortables_init);
 
 function sortables_init() {
