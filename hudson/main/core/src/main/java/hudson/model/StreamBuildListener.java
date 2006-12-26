247d5222857c51c066dbb2ff5662c415e63951bf
kohsuke <kohsuke@jenkins-ci.org>
2006-12-26 15:58:03 +0000

    made remotable.
    git-svn-id: https://svn.jenkins-ci.org/branches/remoting-integration@1441 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/main/core/src/main/java/hudson/model/StreamBuildListener.java
+++ hudson/main/core/src/main/java/hudson/model/StreamBuildListener.java
@@ -1,19 +1,27 @@
 package hudson.model;
 
+import hudson.remoting.RemoteWriter;
 import hudson.util.WriterOutputStream;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.io.Writer;
 
 /**
  * {@link BuildListener} that writes to a {@link Writer}.
+ *
+ * This class is remotable.
+ * 
  * @author Kohsuke Kawaguchi
  */
-public class StreamBuildListener implements BuildListener {
-    private final PrintWriter w;
+public class StreamBuildListener implements BuildListener, Serializable {
+    private PrintWriter w;
 
-    private final PrintStream ps;
+    private PrintStream ps;
 
     public StreamBuildListener(Writer w) {
         this(new PrintWriter(w));
@@ -47,4 +55,16 @@ public class StreamBuildListener implements BuildListener {
     public void finished(Result result) {
         w.println("finished: "+result);
     }
+
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeObject(new RemoteWriter(w));
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        w = new PrintWriter((Writer)in.readObject(),true);
+        ps = new PrintStream(new WriterOutputStream(w),true);
+    }
+
+    private static final long serialVersionUID = 1L;
 }
