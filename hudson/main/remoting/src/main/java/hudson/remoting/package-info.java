fd3462468dc0380f52f00ca1ea5933cbf3c6dacb
unknown <unknown@jenkins-ci.org>
2007-01-04 02:37:06 +0000

    This commit was manufactured by cvs2svn to create tag 'ircbot-1_1'.
    git-svn-id: https://svn.jenkins-ci.org/tags/ircbot-1_1@1619 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/main/remoting/src/main/java/hudson/remoting/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Remoting infrastructure for Hudson.
- *
- * Code in this package is used for running a part of a program in slaves.
- * If you are new to this package, start from {@link Channel}.
- */
-package hudson.remoting;
