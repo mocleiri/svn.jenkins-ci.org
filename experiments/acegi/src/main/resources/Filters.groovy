fc071002c8e34c7859df255792bfaf8cbeb06168
kohsuke <kohsuke@jenkins-ci.org>
2007-11-24 19:12:31 +0000

    noting highlights
    git-svn-id: https://svn.jenkins-ci.org/trunk@6042 71c3de6d-444a-0410-be80-ed276b4c234a

--- experiments/acegi/src/main/resources/Filters.groovy
+++ experiments/acegi/src/main/resources/Filters.groovy
@@ -4,11 +4,12 @@ import org.acegisecurity.ui.ExceptionTranslationFilter
 import org.acegisecurity.ui.basicauth.BasicProcessingFilter
 import org.acegisecurity.ui.basicauth.BasicProcessingFilterEntryPoint
 
-def entryPoint = bean(BasicProcessingFilterEntryPoint) {
-    realmName = "Hudson's security realm"
-}
-
 filter(ChainedServletFilter) {
+    // we can define locally scope bean like this without polluting the global namespace.
+    def entryPoint = bean(BasicProcessingFilterEntryPoint) {
+        realmName = "Hudson's security realm"
+    }
+
     // NOTE: all the variable names, even those that show up LHS,
     // are first resolved as a class name, so if the entire script is
     // named like "filters.groovy", this will fail to compile.
@@ -24,6 +25,8 @@ filter(ChainedServletFilter) {
             userAttribute = "anonymous,"
         },
         bean(ExceptionTranslationFilter) {
+            // property can be created programatically/eagler like this,
+            // instead of doing everything as managed Spring beans
             accessDeniedHandler = new AccessDeniedHandlerImpl()
             authenticationEntryPoint = entryPoint
         }
