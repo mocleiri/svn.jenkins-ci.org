c68221fbbde882a14418d24260c55389544d5779
kohsuke <kohsuke@jenkins-ci.org>
2008-06-06 17:51:14 +0000

    using args4j to customize its behavior through options
    git-svn-id: https://svn.jenkins-ci.org/trunk@9902 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/extras/update-center/src/main/java/org/jvnet/hudson/update_center/Main.java
+++ hudson/extras/update-center/src/main/java/org/jvnet/hudson/update_center/Main.java
@@ -4,6 +4,9 @@ import org.kohsuke.jnt.JNFileFolder;
 import org.kohsuke.jnt.JNProject;
 import org.kohsuke.jnt.JavaNet;
 import org.kohsuke.jnt.ProcessingException;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Argument;
 import net.sf.json.JSONObject;
 
 import javax.xml.rpc.ServiceException;
@@ -12,12 +15,28 @@ import java.io.Writer;
 import java.io.OutputStreamWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.File;
+import java.util.List;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public class Main {
+    @Option(name="-c",metaVar="DIR",usage="Cache directory to store downloaded plugins")
+    public File cacheDir = new File("./cache");
+
+    @Option(name="-o",usage="json file")
+    public String output = "-";
+
     public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        CmdLineParser p = new CmdLineParser(main);
+        p.parseArgument(args);
+
+        main.run();
+    }
+
+    public void run() throws Exception {
         JavaNet jn = JavaNet.connect();
         JNProject p = jn.getProject("hudson");
 
@@ -26,7 +45,7 @@ public class Main {
         root.put("plugins", buildPlugins(p));
         root.put("core", buildCore(p));
 
-        Writer w = args.length==0 ? new OutputStreamWriter(System.out) : new FileWriter(args[0]);
+        Writer w = output.equals("-") ? new OutputStreamWriter(System.out) : new FileWriter(output);
         PrintWriter pw = new PrintWriter(w);
         pw.println("updateCenter.post(");
         root.write(pw);
