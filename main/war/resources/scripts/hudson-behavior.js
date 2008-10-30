507f4fa889314f4debbbd77ac19d1021b16b5c4c
kohsuke <kohsuke@jenkins-ci.org>
2008-10-30 01:01:41 +0000

    added <f:validateButton/> for multi-field server-side validation
    git-svn-id: https://svn.jenkins-ci.org/branches/multiple-computer-per-node@12840 71c3de6d-444a-0410-be80-ed276b4c234a

--- main/war/resources/scripts/hudson-behavior.js
+++ main/war/resources/scripts/hudson-behavior.js
@@ -76,6 +76,35 @@ function findFollowingTR(input, className) {
     return tr;
 }
 
+/**
+ * Traverses a form in the reverse document order starting from the given element (but excluding it),
+ * until the given filter matches, or run out of an element.
+ */
+function findPrevious(src,filter) {
+    function prev(e) {
+        var p = e.previousSibling;
+        if(p==null) return e.parentNode;
+        while(p.lastChild!=null)
+            p = p.lastChild;
+        return p;
+    }
+
+    while(src!=null) {
+        src = prev(src);
+        if(filter(src))
+            return src;
+    }
+    return null;
+}
+/**
+ * Traverses a form in the reverse document order and finds an INPUT element that matches the given name.
+ */
+function findPreviousFormItem(src,name) {
+    var name2 = "_."+name; // handles <textbox field="..." /> notation silently
+    return findPrevious(src,function(e){ return e.tagName=="INPUT" && (e.name==name || e.name==name2); });
+}
+
+
 // shared tooltip object
 var tooltip;
 
@@ -1332,3 +1361,29 @@ var updateCenter = {
         });
     }
 };
+
+// logic behind <f:validateButton />
+function validateButton(checkUrl,paramList,button) {
+  button = button._button;
+
+  var parameters = {};
+
+  paramList.split(',').each(function(name) {
+      var p = findPreviousFormItem(button,name);
+      if(p!=null)
+        parameters[name] = p.value;
+  });
+
+  var spinner = Element.up(button,"DIV").nextSibling;
+  var target = spinner.nextSibling;
+  spinner.style.display="block";
+
+  new Ajax.Request(checkUrl, {
+      method: "post",
+      parameters: parameters,
+      onComplete: function(rsp) {
+          spinner.style.display="none";
+          target.innerHTML = rsp.responseText;
+      }
+  });
+}
