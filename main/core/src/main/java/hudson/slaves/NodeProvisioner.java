befd483921c626603322ff464315c8a51d486365
kohsuke <kohsuke@jenkins-ci.org>
2008-11-11 22:22:03 +0000

    doc improvement and clean up
    git-svn-id: https://svn.jenkins-ci.org/branches/multiple-computer-per-node@13157 71c3de6d-444a-0410-be80-ed276b4c234a

--- main/core/src/main/java/hudson/slaves/NodeProvisioner.java
+++ main/core/src/main/java/hudson/slaves/NodeProvisioner.java
@@ -81,27 +81,51 @@ public class NodeProvisioner extends SafeTimerTask {
         // TODO: do this for each label separately.
         LoadStatistics stat = hudson.overallLoad;
 
-        float idle = stat.getLatestIdleExecutors(picker);
-        if(idle<MARGIN) {// the system is fully utilized
-            // make a conservative estimate of queue length
-            // stat.queueLength is the exponential moving average, so when there's a temporal queue length surge,
-            // it increases slowly, which helps us avoid over-reaction.
-            // OTOH, when the Q length goes down because of new capacity, the moving average decreases only slowly,
-            // while planned capacity goes down immediately, so this creates phantom excessive workload.
-            //
-            // so we look at the current Q length as well and take a smaller value, to be conservative.
-            // in a long run when nothing changes, the moving average and the snapshot value is identical,
-            // so this ensures that we won't have something in Q forever waiting for an extra capacity.
+        /*
+            Here we determine how many additional slaves we need to keep up with the load (if at all),
+            which involves a simple math.
+
+            Broadly speaking, first we check that all the executors are fully utilized before attempting
+            to start any new slave (this also helps to ignore the temporary gap between different numbers,
+            as changes in them are not necessarily synchronized --- for example, there's a time lag between
+            when a slave launches (thus bringing the planned capacity down) and the time when its executors
+            pick up builds (thus bringing the queue length down.)
+
+            Once we confirm that, we compare the # of buildable items against the additional slaves
+            that are being brought online. If we have more jobs than our executors can handle, we'll launch a new slave.
+
+            So this computation involves three stats:
+
+              1. # of idle executors
+              2. # of jobs that are starving for executors
+              3. # of additional slaves being provisioned (planned capacities.)
+
+            To ignore a temporary surge/drop, we make conservative estimates on each one of them. That is,
+            we take the current snapshot value, and we take the current exponential moving average (EMA) value,
+            and use the max/min.
+
+            This is another measure to be robust against temporary surge/drop in those indicators, and helps
+            us avoid over-reacting to stats.
+
+            If we only use the snapshot value or EMA value, tests confirmed that the gap creates phantom
+            excessive loads and Hudson ends up firing excessive capacities. In a static system, over the time
+            EMA and the snapshot value becomes the same, so this makes sure that in a long run this conservative
+            estimate won't create a starvation.
+         */
+
+        float idle = Math.min(stat.getLatestIdleExecutors(picker),hudson.getComputer().getIdleExecutors());
+        if(idle<MARGIN) {
+            // make sure the system is fully utilized before attempting any new launch.
+
+            // this is the amount of work left to be done
             float qlen = Math.min(stat.queueLength.getLatest(picker), hudson.getQueue().getBuildableItems().size());
 
-            // when we subtract plannedCapacity from qlen below, also make a conservative estimate
-            // by mixing in  the EMA of planned capacity. This prevents a temporary glitch from allocating
-            // more slaves.
+            // ... and this is the additional executors we've already provisioned.
             plannedCapacity = Math.max(plannedCapacitiesEMA.getLatest(picker),plannedCapacity);
 
             float excessWorkload = qlen - plannedCapacity;
-            if(excessWorkload>1-MARGIN) {// and there's more work to do than we are currently bringing up
-                LOGGER.fine("Excess workload "+excessWorkload+" detected. (planned capacity="+plannedCapacity+",Qlen="+qlen+")");
+            if(excessWorkload>1-MARGIN) {// and there's more work to do...
+                LOGGER.info("Excess workload "+excessWorkload+" detected. (planned capacity="+plannedCapacity+",Qlen="+qlen+")");
                 for( Cloud c : hudson.clouds ) {
                     if(excessWorkload<0)    break;  // enough slaves allocated
 
@@ -113,7 +137,7 @@ public class NodeProvisioner extends SafeTimerTask {
 
                     Collection<PlannedNode> additionalCapacities = c.provision((int)Math.round(Math.floor(excessWorkload+MARGIN)));
                     for (PlannedNode ac : additionalCapacities) {
-                        LOGGER.info(ac.displayName+" provisioned from "+c.name+" with "+ac.numExecutors+" (remaining excess workload:"+excessWorkload+")");
+                        LOGGER.info("Provisioned "+ac.displayName+" from "+c.name+" with "+ac.numExecutors+" executors. Remaining excess workload:"+excessWorkload);
                         excessWorkload -= ac.numExecutors;
                     }
                     pendingLaunches.addAll(additionalCapacities);
