f78a0d743a979f12b33d634326482d458557e81f
kohsuke <kohsuke@jenkins-ci.org>
2008-11-11 02:20:19 +0000

    adding another test case
    git-svn-id: https://svn.jenkins-ci.org/branches/multiple-computer-per-node@13147 71c3de6d-444a-0410-be80-ed276b4c234a

--- main/test/src/test/java/hudson/slaves/NodeProvisionerTest.java
+++ main/test/src/test/java/hudson/slaves/NodeProvisionerTest.java
@@ -12,10 +12,9 @@ import org.jvnet.hudson.test.SleepBuilder;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.logging.Logger;
-import java.util.logging.Level;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
 import java.io.IOException;
 
 /**
@@ -66,26 +65,31 @@ public class NodeProvisionerTest extends HudsonTestCase {
         try {
             DummyCloudImpl cloud = initHudson(0);
 
-            List<FreeStyleProject> jobs = new ArrayList<FreeStyleProject>();
-            for( int i=0; i<10; i++)
-                //set a large delay, to simulate the situation where we need to provision more slaves
-                // to keep up with the load  
-                jobs.add(createJob(3000));
-
-            System.out.println("Scheduling a build");
-            List<Future<FreeStyleBuild>> builds = new ArrayList<Future<FreeStyleBuild>>();
-            for (FreeStyleProject job : jobs)
-                builds.add(job.scheduleBuild2(0));
-
-            System.out.println("Waiting for a completion");
-            for (Future<FreeStyleBuild> f : builds) {
-                FreeStyleBuild b = f.get();// if it's taking too long, abort.
-                assertEquals(Result.SUCCESS,b.getResult());
-            }
+            verifySuccessfulCompletion(buildAll(create10SlowJobs()));
 
             // the time it takes to complete a job is eternally long compared to the time it takes to launch
             // a new slave, so in this scenario we end up allocating 10 slaves for 10 jobs.
             assertEquals(10,cloud.numProvisioned);
+        } finally {
+            bc.abort();
+        }
+    }
+
+    /**
+     * Scenario: make sure we take advantage of statically configured slaves.
+     */
+    public void testBaselineSlaveUsage() throws Exception {
+        BulkChange bc = new BulkChange(hudson);
+        try {
+            DummyCloudImpl cloud = initHudson(0);
+            // add slaves statically upfront
+            createSlave();
+            createSlave();
+
+            verifySuccessfulCompletion(buildAll(create10SlowJobs()));
+
+            // we should have used two static slaves, thus only 8 slaves should have been provisioned
+            assertEquals(8,cloud.numProvisioned);
         } finally {
             bc.abort();
         }
@@ -108,5 +112,33 @@ public class NodeProvisionerTest extends HudsonTestCase {
         hudson.setNumExecutors(0);
         hudson.setNodes(Collections.<Node>emptyList());
         return cloud;
+    }
+
+    private List<FreeStyleProject> create10SlowJobs() throws IOException {
+        List<FreeStyleProject> jobs = new ArrayList<FreeStyleProject>();
+        for( int i=0; i<10; i++)
+            //set a large delay, to simulate the situation where we need to provision more slaves
+            // to keep up with the load
+            jobs.add(createJob(3000));
+        return jobs;
+    }
+
+    /**
+     * Builds all the given projects at once.
+     */
+    private List<Future<FreeStyleBuild>> buildAll(List<FreeStyleProject> jobs) {
+        System.out.println("Scheduling a build");
+        List<Future<FreeStyleBuild>> builds = new ArrayList<Future<FreeStyleBuild>>();
+        for (FreeStyleProject job : jobs)
+            builds.add(job.scheduleBuild2(0));
+        return builds;
+    }
+
+    private void verifySuccessfulCompletion(List<Future<FreeStyleBuild>> builds) throws InterruptedException, ExecutionException {
+        System.out.println("Waiting for a completion");
+        for (Future<FreeStyleBuild> f : builds) {
+            FreeStyleBuild b = f.get();// if it's taking too long, abort.
+            assertEquals(Result.SUCCESS,b.getResult());
+        }
     }
 }
