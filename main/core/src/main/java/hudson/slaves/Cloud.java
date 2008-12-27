ceaf702ccd4beeaee929069ef608f8d8c36424a1
kohsuke <kohsuke@jenkins-ci.org>
2008-12-27 15:54:53 +0000

    doc improvement
    git-svn-id: https://svn.jenkins-ci.org/branches/multiple-computer-per-node@13898 71c3de6d-444a-0410-be80-ed276b4c234a

--- main/core/src/main/java/hudson/slaves/Cloud.java
+++ main/core/src/main/java/hudson/slaves/Cloud.java
@@ -17,7 +17,12 @@ import java.util.Collection;
 /**
  * Creates {@link Node}s to dynamically expand/shrink the slaves attached to Hudson.
  *
+ * <p>
+ * Put another way, this class encapsulates different communication protocols
+ * needed to start a new slave programmatically.
+ *
  * @author Kohsuke Kawaguchi
+ * @see NodeProvisioner
  */
 public abstract class Cloud extends AbstractModelObject implements ExtensionPoint, Describable<Cloud>, AccessControlled {
 
@@ -52,6 +57,30 @@ public abstract class Cloud extends AbstractModelObject implements ExtensionPoin
 
     /**
      * Provisions new {@link Node}s from this cloud.
+     *
+     * <p>
+     * {@link NodeProvisioner} performs a trend analysis on the load,
+     * and when it determines that it <b>really</b> needs to bring up
+     * additional nodes, this method is invoked.
+     *
+     * <p>
+     * The implementation of this method asynchronously starts
+     * node provisioning.
+     *
+     * @param label
+     *      The label that indicates what kind of nodes are needed now.
+     *      Newly launched node needs to have this label.
+     *      Only those {@link Label}s that this instance returned true
+     *      from the {@link #canProvision(Label)} method will be passed here.
+     * @param excessWorkload
+     *      Number of total executors needed to meet the current demand.
+     *      Always >= 1. For example, if this is 3, the implementation
+     *      should launch 3 slaves with 1 executor each, or 1 slave with
+     *      3 executors, etc.
+     *
+     * @return
+     *      {@link PlannedNode}s that represent asynchronous {@link Node}
+     *      launch operations. Can be empty but must not be null.
      */
     public abstract Collection<PlannedNode> provision(Label label, int excessWorkload);
 
