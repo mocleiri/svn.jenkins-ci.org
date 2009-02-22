6e2a40e3cfe4682206f0d3833347e152e7cfbd96
kohsuke <kohsuke@jenkins-ci.org>
2009-02-22 05:00:29 +0000

    moving WMI support into another library, and making more progress
    git-svn-id: https://svn.jenkins-ci.org/branches/managed-windows-slave@15540 71c3de6d-444a-0410-be80-ed276b4c234a

--- core/src/main/java/hudson/os/windows/ManagedWindowsServiceLauncher.java
+++ core/src/main/java/hudson/os/windows/ManagedWindowsServiceLauncher.java
@@ -23,19 +23,35 @@
  */
 package hudson.os.windows;
 
+import hudson.lifecycle.WindowsSlaveInstaller;
+import hudson.model.Descriptor;
+import hudson.model.Hudson;
 import hudson.slaves.ComputerLauncher;
 import hudson.slaves.SlaveComputer;
 import hudson.util.StreamTaskListener;
-import hudson.model.Descriptor;
-import hudson.model.Hudson;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.InputStream;
-
+import hudson.util.Secret;
+import hudson.util.jna.DotNet;
 import jcifs.smb.SmbFile;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.jinterop.dcom.common.JIException;
+import org.jinterop.dcom.common.JIDefaultAuthInfoImpl;
+import org.jinterop.dcom.core.JISession;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.jvnet.hudson.wmi.WMI;
+import org.jvnet.hudson.wmi.SWbemServices;
+import org.jvnet.hudson.wmi.Win32Service;
+import static org.jvnet.hudson.wmi.Win32Service.Win32OwnProcess;
+import org.dom4j.io.SAXReader;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.PrintStream;
+import java.net.UnknownHostException;
 
 /**
  * Windows slave installed/managed as a service entirely remotely
@@ -43,20 +59,106 @@ import org.apache.commons.io.IOUtils;
  * @author Kohsuke Kawaguchi
  */
 public class ManagedWindowsServiceLauncher extends ComputerLauncher {
+    /**
+     * "[DOMAIN\\]USERNAME" to follow the Windows convention.
+     */
+    public final String userName;
+    
+    public final Secret password;
+
+    @DataBoundConstructor
+    public ManagedWindowsServiceLauncher(String userName, String password) {
+        this.userName = userName;
+        this.password = Secret.fromString(password);
+    }
+
+    private JIDefaultAuthInfoImpl createAuth() {
+        String[] tokens = userName.split("\\\\");
+        if(tokens.length==2)
+            return new JIDefaultAuthInfoImpl(tokens[0], tokens[1], password.toString());
+        return new JIDefaultAuthInfoImpl("", userName, password.toString());
+    }
+
     public void launch(SlaveComputer computer, StreamTaskListener listener) throws IOException, InterruptedException {
-        String path = computer.getNode().getRemoteFS();
-        // copy slave.jar
-        SmbFile remoteRoot = new SmbFile("smb://" + computer.getName() + "/" + path.replace('\\', '/').replace(':', '$'));
+        try {
+            PrintStream logger = listener.getLogger();
 
-        // copy exe
-        copyAndClose(getClass().getResource("/windows-service/hudson.exe").openStream(),
-                new SmbFile(remoteRoot,"hudson-slave.exe").getOutputStream());
+            logger.println("Connecting to "+computer.getName());
+            JIDefaultAuthInfoImpl auth = createAuth();
+            JISession session = JISession.createSession(auth);
+            session.setGlobalSocketTimeout(60000);
+            SWbemServices services = WMI.connect(session, computer.getName());
 
-        // copy slave.jar
-        copyAndClose(Hudson.getInstance().getJnlpJars("slave.jar").getURL().openStream(),
-                new SmbFile(remoteRoot,"hudson-slave.exe").getOutputStream());
-        // TODO
-        throw new UnsupportedOperationException();
+            Win32Service slaveService = services.getService("hudsonslave");
+            if(slaveService==null) {
+                logger.println("Installing the Hudson slave service");
+                if(!DotNet.isInstalled(2,0,computer.getName(), auth)) {
+                    // abort the launch
+                    logger.println(".NET Framework 2.0 or later is required to run a Hudson slave as a Windows service");
+                    return;
+                }
+    
+                String path = computer.getNode().getRemoteFS();
+                SmbFile remoteRoot = new SmbFile("smb://" + computer.getName() + "/" + path.replace('\\', '/').replace(':', '$'));
+
+                // copy exe
+                logger.println("Copying hudson-slave.exe");
+                copyAndClose(getClass().getResource("/windows-service/hudson.exe").openStream(),
+                        new SmbFile(remoteRoot,"hudson-slave.exe").getOutputStream());
+
+                // copy slave.jar
+                logger.println("Copying slave.jar");
+                copyAndClose(Hudson.getInstance().getJnlpJars("slave.jar").getURL().openStream(),
+                        new SmbFile(remoteRoot,"slave.jar").getOutputStream());
+
+                // copy hudson-slave.xml
+                logger.println("Copying hudson-slave.xml");
+                String xml = WindowsSlaveInstaller.generateSlaveXml("java.exe",Hudson.getInstance().getRootUrl()+computer.getUrl()+"slave-agent.jnlp");
+                copyAndClose(new ByteArrayInputStream(xml.getBytes("UTF-8")),
+                        new SmbFile(remoteRoot,"hudson-slave.xml").getOutputStream());
+
+                // install it as a service
+                logger.println("Registering the service");
+                Document dom = new SAXReader().read(new StringReader(xml));
+                Win32Service svc = services.Get("Win32_Service").cast(Win32Service.class);
+                int r = svc.Create(
+                        dom.selectSingleNode("/service/id").getText(),
+                        dom.selectSingleNode("/service/name").getText(),
+                        "java "+dom.selectSingleNode("/service/arguments").getText(),
+                        Win32OwnProcess, 0, "Auto", true);
+                if(r!=0) {
+                    listener.error("Failed to create a service. Error code="+r);
+                    return;
+                }
+                slaveService = services.getService("hudsonslave");
+            }
+
+            logger.println("Starting the service");
+            slaveService.StartService();
+        } catch (JIException e) {
+            e.printStackTrace(listener.error(e.getMessage()));
+        } catch (DocumentException e) {
+            e.printStackTrace(listener.error(e.getMessage()));
+        }
+    }
+
+    @Override
+    public void afterDisconnect(SlaveComputer computer, StreamTaskListener listener) {
+        try {
+            JIDefaultAuthInfoImpl auth = createAuth();
+            JISession session = JISession.createSession(auth);
+            session.setGlobalSocketTimeout(60000);
+            SWbemServices services = WMI.connect(session, computer.getName());
+            Win32Service slaveService = services.getService("hudsonslave");
+            if(slaveService!=null) {
+                listener.getLogger().println("Stopping the service");
+                slaveService.StopService();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace(listener.error(e.getMessage()));
+        } catch (JIException e) {
+            e.printStackTrace(listener.error(e.getMessage()));
+        }
     }
 
     private static void copyAndClose(InputStream in, OutputStream out) {
