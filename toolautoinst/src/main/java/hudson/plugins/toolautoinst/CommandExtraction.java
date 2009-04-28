0fae73e882a081cc73db2b0dd9ac194f3924d777
jglick <jglick@jenkins-ci.org>
2009-04-28 03:07:47 +0000

    Second tool installer type based on a freeform command.
    git-svn-id: https://svn.jenkins-ci.org/branches/tool-autoinstallation@17559 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ toolautoinst/src/main/java/hudson/plugins/toolautoinst/CommandExtraction.java
@@ -0,0 +1,86 @@
+package hudson.plugins.toolautoinst;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.tasks.CommandInterpreter;
+import hudson.tools.ToolInstallation;
+import hudson.util.FormValidation;
+import java.io.IOException;
+import java.util.Collections;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
+/**
+ * Installs a tool by running an arbitrary shell command.
+ */
+public class CommandExtraction extends ToolInstaller {
+
+    /**
+     * Command to execute, similar to {@link CommandInterpreter#command}.
+     */
+    private final String command;
+
+    /**
+     * Resulting tool home directory.
+     */
+    private final String toolHome;
+
+    @DataBoundConstructor
+    public CommandExtraction(String toolName, String label, String command, String toolHome) {
+        super(toolName, label);
+        this.command = command;
+        this.toolHome = toolHome;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getToolHome() {
+        return toolHome;
+    }
+
+    public FilePath performInstallation(ToolInstallation tool, Node node, TaskListener log) throws IOException, InterruptedException {
+        FilePath tools = node.getRootPath().child("tools");
+        // XXX support Windows batch scripts, Unix scripts with interpreter line, etc. (see CommandInterpreter subclasses)
+        FilePath script = tools.createTextTempFile("hudson", ".sh", command);
+        try {
+            String[] cmd = {"sh", "-xe", script.getRemote()};
+            int r = node.createLauncher(log).launch(cmd, Collections.<String,String>emptyMap(), log.getLogger(), tools).join();
+            if (r != 0) {
+                throw new IOException("Command returned status " + r);
+            }
+        } finally {
+            script.delete();
+        }
+        return node.createPath(toolHome);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ToolInstallerDescriptor<CommandExtraction> {
+
+        public String getDisplayName() {
+            return "Run Command"; // XXX I18N
+        }
+
+        public FormValidation doCheckCommand(@QueryParameter String value) {
+            if (value.length() > 0) {
+                return FormValidation.ok();
+            } else {
+                return FormValidation.error("Must provide a command to run."); // XXX I18N
+            }
+        }
+
+        public FormValidation doCheckToolHome(@QueryParameter String value) {
+            if (value.length() > 0) {
+                return FormValidation.ok();
+            } else {
+                return FormValidation.error("Must provide a tool home directory."); // XXX I18N
+            }
+        }
+
+    }
+
+}
