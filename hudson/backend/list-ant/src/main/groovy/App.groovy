43eb15c19e9b716e3c750b07ca1c184c860403bc
kohsuke <kohsuke@jenkins-ci.org>
2009-05-08 05:21:58 +0000

    adding Maven here, since there's so much reuse
    git-svn-id: https://svn.jenkins-ci.org/trunk@17863 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/backend/list-ant/src/main/groovy/App.groovy
+++ hudson/backend/list-ant/src/main/groovy/App.groovy
@@ -4,31 +4,36 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage
 import java.util.regex.Pattern
 import net.sf.json.JSONObject
 
-/**
- * Generate a file that lists all Ant releases.
- */
-wc = new WebClient()
-wc.setJavaScriptEnabled(false);
-wc.setCssEnabled(false);
-HtmlPage p = (HtmlPage)wc.getPage("http://archive.apache.org/dist/ant/binaries/");
+def listUp(url,pattern) {
+    wc = new WebClient()
+    wc.setJavaScriptEnabled(false);
+    wc.setCssEnabled(false);
+    HtmlPage p = wc.getPage(url);
+    pattern=Pattern.compile(pattern);
 
-pattern=Pattern.compile("ant-(.+)-bin.zip");
+    return p.getAnchors().collect { HtmlAnchor a ->
+        m = pattern.matcher(a.hrefAttribute)
+        if(m.find()) {
+            ver=m.group(1)
+            url = p.getFullyQualifiedUrl(a.hrefAttribute);
+            return ["version":ver, "url":url.toExternalForm()]
+        }
+        return null;
+    }.findAll { it!=null }
+}
 
-versions = p.getAnchors().findAll{ a -> a.getTextContent().endsWith("-bin.zip")}.collect { HtmlAnchor a ->
-    m = pattern.matcher(a.hrefAttribute)
-    if(m.find()) {
-        ver=m.group(1)
-        url = p.getFullyQualifiedUrl(a.hrefAttribute);
-        return ["version":ver, "url":url.toExternalForm()]
+def store(key,jsonp,o) {
+    JSONObject envelope = JSONObject.fromObject(["${key}": o]);
+    println envelope.toString(2)
+
+    if(project!=null) {
+        // if we run from GMaven during a build, put that out in a file as well, with the JSONP support
+        File d = new File(project.basedir, "target")
+        d.mkdirs()
+        new File(d,"${key}.json").write("${jsonp}(${envelope.toString()})");
     }
 }
 
-JSONObject envelope = JSONObject.fromObject(["ant": versions])
-println envelope.toString(2)
 
-if(project!=null) {
-    // if we run from GMaven during a build, put that out in a file as well, with the JSONP support
-    File d = new File(project.basedir, "target")
-    d.mkdirs()
-    new File(d,"ant.json").write("listOfAnts(${envelope.toString()})");
-}
+store("ant","listOfAnts",    listUp("http://archive.apache.org/dist/ant/binaries/","ant-(.+)-bin.zip\$"))
+store("maven","listOfMavens",listUp("http://archive.apache.org/dist/maven/binaries/","maven-([0-9.]+)(-bin)?.zip\$"))
