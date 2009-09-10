d31d0114c498da6f9646b7e0346f7ddf888a6c86
drulli <drulli@jenkins-ci.org>
2009-09-10 21:42:13 +0000

    [maven-scm] copy for tag checkstyle-2.14
    git-svn-id: https://svn.jenkins-ci.org/tags/checkstyle-2.14@21581 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/xunit/src/test/resources/com/thalesgroup/hudson/plugins/xunit/transformer/boosttest/cmd.cmd
+++
@@ -1,6 +0,0 @@
-./a.exe --report_format=XML --report_level=detailed
-
-./a.exe --output_format=XML --log_level=all
-
-Correct for plugin with only testlog
-./a.exe --output_format=XML --log_level=all --report_level=no
