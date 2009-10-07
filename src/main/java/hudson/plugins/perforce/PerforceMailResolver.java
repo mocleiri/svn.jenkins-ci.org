52452ee0b28fda4d93e19661e7bc4cca55073516
rpetti <rpetti@jenkins-ci.org>
2009-10-07 14:58:22 +0000

    fixing left-open connection in mail resolver
    git-svn-id: https://svn.jenkins-ci.org/branches/perforce-p4java@22490 71c3de6d-444a-0410-be80-ed276b4c234a

--- src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
+++ src/main/java/hudson/plugins/perforce/PerforceMailResolver.java
@@ -1,5 +1,6 @@
 package hudson.plugins.perforce;
 
+import com.perforce.p4java.server.P4JServer;
 import hudson.model.AbstractProject;
 import hudson.model.User;
 import hudson.tasks.MailAddressResolver;
@@ -22,7 +23,9 @@ public class PerforceMailResolver extends MailAddressResolver {
                 PerforceSCM pscm = (PerforceSCM) p.getScm();
                 try {
                     // couldn't resist the name pu...
-                    P4JUser pu = P4jUtil.getUser(pscm.getServer(), u.getId());
+                    P4JServer server = pscm.getServer();
+                    P4JUser pu = P4jUtil.getUser(server, u.getId());
+                    server.disconnect();
                     if (pu.getEmail() != null && !pu.getEmail().equals(""))
                         return pu.getEmail();
                 } catch (Exception e) {
