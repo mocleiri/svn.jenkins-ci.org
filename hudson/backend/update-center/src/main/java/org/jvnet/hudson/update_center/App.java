3acef86555c19ba61352d6a0ebeaae7656bb51b8
kohsuke <kohsuke@jenkins-ci.org>
2009-10-16 01:31:31 +0000

    magical working combination.
    git-svn-id: https://svn.jenkins-ci.org/trunk@22814 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/backend/update-center/src/main/java/org/jvnet/hudson/update_center/App.java
+++ hudson/backend/update-center/src/main/java/org/jvnet/hudson/update_center/App.java
@@ -1,42 +1,39 @@
 package org.jvnet.hudson.update_center;
 
-import org.codehaus.plexus.classworlds.ClassWorld;
-import org.codehaus.plexus.ContainerConfiguration;
-import org.codehaus.plexus.DefaultContainerConfiguration;
-import org.codehaus.plexus.PlexusContainer;
-import org.codehaus.plexus.DefaultPlexusContainer;
-import org.sonatype.nexus.index.NexusIndexer;
-import org.sonatype.nexus.index.ArtifactInfo;
-import org.sonatype.nexus.index.FlatSearchRequest;
-import org.sonatype.nexus.index.FlatSearchResponse;
-import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.BooleanClause.Occur;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.transform.ArtifactTransformationManager;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
 import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
 import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.transform.ArtifactTransformationManager;
 import org.apache.maven.embedder.MavenEmbedder;
-import org.apache.maven.embedder.DefaultConfiguration;
-import org.apache.maven.embedder.ContainerCustomizer;
+import org.codehaus.plexus.ContainerConfiguration;
+import org.codehaus.plexus.DefaultContainerConfiguration;
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.classworlds.ClassWorld;
+import org.sonatype.nexus.index.ArtifactInfo;
+import org.sonatype.nexus.index.FlatSearchRequest;
+import org.sonatype.nexus.index.FlatSearchResponse;
+import org.sonatype.nexus.index.NexusIndexer;
 
 import java.io.File;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.TreeSet;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * Testing Nexus indexer based plugin discovery, as a part of the effort to move away from java.net.
+ *
+ * Using Maven embedder 2.0.4 results in problem caused by Plexus incompatibility.
  *
  * @author Kohsuke Kawaguchi
  */
@@ -80,24 +77,18 @@ public class App {
         FlatSearchRequest request = new FlatSearchRequest(q);
         FlatSearchResponse response = indexer.searchFlat(request);
 
-        DefaultConfiguration config = new DefaultConfiguration();
-        config.setConfigurationCustomizer(new ContainerCustomizer() {
-            public void customize(PlexusContainer container) {
-                container.getComponentDescriptor(ArtifactTransformationManager.class,
-                        ArtifactTransformationManager.class.getName(),"default").setImplementationClass(DefaultArtifactTransformationManager.class);
-            }
-        });
-        MavenEmbedder embedder = new MavenEmbedder(config);
+        ArtifactFactory af = plexus.lookup(ArtifactFactory.class);
+        ArtifactResolver ar = plexus.lookup(ArtifactResolver.class);
+        ArtifactRepositoryFactory arf = plexus.lookup(ArtifactRepositoryFactory.class);
 
-        ArtifactRepositoryFactory arf = embedder.getPlexusContainer().lookup(ArtifactRepositoryFactory.class);
         ArtifactRepositoryPolicy policy = new ArtifactRepositoryPolicy(true, "daily", "warn");
-        List<ArtifactRepository> repos =
-                    Collections.singletonList(arf.createArtifactRepository("m.g.o-public","http://maven.glassfish.org/content/groups/public/",new DefaultRepositoryLayout(), policy, policy));
+        List<ArtifactRepository> remoteRepositories = Collections.singletonList(
+                arf.createArtifactRepository("m.g.o-public", "http://maven.glassfish.org/content/groups/public/",
+                        new DefaultRepositoryLayout(), policy, policy));
 
-//        ArtifactFactory af = plexus.lookup(ArtifactFactory.class);
-//        ArtifactResolver ar = plexus.lookup(ArtifactResolver.class);
-//        ArtifactRepositoryFactory arf = plexus.lookup(ArtifactRepositoryFactory.class);
-
+        ArtifactRepository local = arf.createArtifactRepository("local",
+                new File(new File(System.getProperty("user.home")), ".m2/repository").toURI().toURL().toExternalForm(),
+                new DefaultRepositoryLayout(), policy, policy);
 
         Map<String,Group> plugins = new TreeMap<String,Group>();
 
@@ -110,13 +101,9 @@ public class App {
             p.artifacts.add(a);
             p.groupId.add(a.groupId);
 
-//            Artifact artifact = af.createArtifact(a.groupId, a.artifactId, a.version, "compile", a.packaging);
-//            ArtifactRepositoryPolicy policy = new ArtifactRepositoryPolicy(true, "daily", "warn");
-//            ar.resolve(artifact,
-//                    Collections.singletonList(arf.createArtifactRepository("m.g.o-public","http://maven.glassfish.org/content/groups/public/",new DefaultRepositoryLayout(), policy, policy));
+            Artifact artifact = af.createArtifact(a.groupId, a.artifactId, a.version, "compile", a.packaging);
 
-            Artifact artifact = embedder.createArtifact(a.groupId, a.artifactId, a.version, "compile", a.packaging);
-            embedder.resolve(artifact, repos, embedder.getLocalRepository());
+            ar.resolve(artifact,remoteRepositories,local);
 
             System.out.println(artifact.getFile());
         }
