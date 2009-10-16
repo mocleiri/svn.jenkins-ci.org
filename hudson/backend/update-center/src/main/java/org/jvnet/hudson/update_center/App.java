b504476a6353f33e8c108ca113fe05a63cc54a8d
kohsuke <kohsuke@jenkins-ci.org>
2009-10-16 00:58:30 +0000

    using Maven embedder 3.0 fails to find org.apache.maven.artifact.manager.WagonManager.findAndRegisterWagons(Lorg/codehaus/plexus/PlexusContainer;)V
    git-svn-id: https://svn.jenkins-ci.org/trunk@22813 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/backend/update-center/src/main/java/org/jvnet/hudson/update_center/App.java
+++ hudson/backend/update-center/src/main/java/org/jvnet/hudson/update_center/App.java
@@ -11,6 +11,19 @@ import org.sonatype.nexus.index.FlatSearchRequest;
 import org.sonatype.nexus.index.FlatSearchResponse;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.transform.ArtifactTransformationManager;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.embedder.MavenEmbedder;
+import org.apache.maven.embedder.DefaultConfiguration;
+import org.apache.maven.embedder.ContainerCustomizer;
 
 import java.io.File;
 import java.util.Map;
@@ -18,6 +31,9 @@ import java.util.TreeMap;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.TreeSet;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Testing Nexus indexer based plugin discovery, as a part of the effort to move away from java.net.
@@ -48,9 +64,12 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        ClassWorld classWorld = new ClassWorld( "plexus.core", Thread.currentThread().getContextClassLoader() );
+        ClassWorld classWorld = new ClassWorld( "plexus.core", App.class.getClassLoader() );
         ContainerConfiguration configuration = new DefaultContainerConfiguration().setClassWorld( classWorld );
         PlexusContainer plexus = new DefaultPlexusContainer( configuration );
+        plexus.getComponentDescriptor(ArtifactTransformationManager.class,
+                ArtifactTransformationManager.class.getName(),"default").setImplementationClass(DefaultArtifactTransformationManager.class);
+
 
         NexusIndexer indexer = plexus.lookup( NexusIndexer.class );
         indexer.addIndexingContext("java.net2","java.net2",null,new File("index"),null,null, NexusIndexer.DEFAULT_INDEX);
@@ -60,6 +79,25 @@ public class App {
 
         FlatSearchRequest request = new FlatSearchRequest(q);
         FlatSearchResponse response = indexer.searchFlat(request);
+
+        DefaultConfiguration config = new DefaultConfiguration();
+        config.setConfigurationCustomizer(new ContainerCustomizer() {
+            public void customize(PlexusContainer container) {
+                container.getComponentDescriptor(ArtifactTransformationManager.class,
+                        ArtifactTransformationManager.class.getName(),"default").setImplementationClass(DefaultArtifactTransformationManager.class);
+            }
+        });
+        MavenEmbedder embedder = new MavenEmbedder(config);
+
+        ArtifactRepositoryFactory arf = embedder.getPlexusContainer().lookup(ArtifactRepositoryFactory.class);
+        ArtifactRepositoryPolicy policy = new ArtifactRepositoryPolicy(true, "daily", "warn");
+        List<ArtifactRepository> repos =
+                    Collections.singletonList(arf.createArtifactRepository("m.g.o-public","http://maven.glassfish.org/content/groups/public/",new DefaultRepositoryLayout(), policy, policy));
+
+//        ArtifactFactory af = plexus.lookup(ArtifactFactory.class);
+//        ArtifactResolver ar = plexus.lookup(ArtifactResolver.class);
+//        ArtifactRepositoryFactory arf = plexus.lookup(ArtifactRepositoryFactory.class);
+
 
         Map<String,Group> plugins = new TreeMap<String,Group>();
 
@@ -71,6 +109,16 @@ public class App {
                 plugins.put(a.artifactId, p=new Group(a.artifactId));
             p.artifacts.add(a);
             p.groupId.add(a.groupId);
+
+//            Artifact artifact = af.createArtifact(a.groupId, a.artifactId, a.version, "compile", a.packaging);
+//            ArtifactRepositoryPolicy policy = new ArtifactRepositoryPolicy(true, "daily", "warn");
+//            ar.resolve(artifact,
+//                    Collections.singletonList(arf.createArtifactRepository("m.g.o-public","http://maven.glassfish.org/content/groups/public/",new DefaultRepositoryLayout(), policy, policy));
+
+            Artifact artifact = embedder.createArtifact(a.groupId, a.artifactId, a.version, "compile", a.packaging);
+            embedder.resolve(artifact, repos, embedder.getLocalRepository());
+
+            System.out.println(artifact.getFile());
         }
 
         for (Map.Entry<String,Group> e : plugins.entrySet()) {
