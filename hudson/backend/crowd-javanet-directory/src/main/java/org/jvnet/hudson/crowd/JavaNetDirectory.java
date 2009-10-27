0c2c1f74c3dc0386d92705f17c2d4aa1f9374b91
kohsuke <kohsuke@jenkins-ci.org>
2009-10-27 22:22:05 +0000

    JIRA needs to be able to list up all users, as well as all users in a certain group.
    git-svn-id: https://svn.jenkins-ci.org/trunk@23249 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/backend/crowd-javanet-directory/src/main/java/org/jvnet/hudson/crowd/JavaNetDirectory.java
+++ hudson/backend/crowd-javanet-directory/src/main/java/org/jvnet/hudson/crowd/JavaNetDirectory.java
@@ -7,26 +7,31 @@ import com.atlassian.crowd.integration.exception.ObjectNotFoundException;
 import com.atlassian.crowd.integration.model.group.Group;
 import com.atlassian.crowd.integration.model.group.GroupWithAttributes;
 import com.atlassian.crowd.integration.model.user.User;
-import com.atlassian.crowd.integration.model.user.UserWithAttributes;
-import com.atlassian.crowd.search.query.entity.EntityQuery;
-import com.atlassian.crowd.search.query.membership.MembershipQuery;
 import com.atlassian.crowd.search.Entity;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
-import static java.util.Arrays.asList;
-
-import org.kohsuke.jnt.ProcessingException;
+import com.atlassian.crowd.search.query.entity.EntityQuery;
+import com.atlassian.crowd.search.query.entity.restriction.NullRestriction;
+import com.atlassian.crowd.search.query.membership.MembershipQuery;
 import org.kohsuke.jnt.JavaNet;
+import org.kohsuke.jnt.ProcessingException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import static java.util.Arrays.asList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Adapter to expose java.net user database to Crowd.
  *
  * <p>
- * This has some hard-coded knowledge about groups that Confluence expects.
+ * This has some hard-coded knowledge about groups that Confluence/JIRA expects.
  *
  * @see <a href="http://confluence.atlassian.com/display/CROWD/Creating+a+Custom+Directory+Connector">guide</a>
  * @author Kohsuke Kawaguchi
@@ -41,20 +46,31 @@ public class JavaNetDirectory extends AbstractRemoteDirectory {
 
     private final ConcurrentHashMap<String,String> authCache = new ConcurrentHashMap<String, String>();
 
+    private final Map<String,UserImpl> allUsers = new HashMap<String,UserImpl>();
+
     public JavaNetDirectory() {
         for (GroupImpl g : asList(confluenceUsers,confluenceAdministrators,jiraUsers,jiraDevelopers,jiraAdministrators))
             groups.put(g.getName(), g);
+
+        try {
+            BufferedReader r = new BufferedReader(new FileReader("/home/crowd/users.txt"));
+            String name;
+            while ((name=r.readLine())!=null)
+                allUsers.put(name,findUserByName(name));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getDescriptiveName() {
         return "java.net user database";
     }
 
-    public User findUserByName(String name) throws ObjectNotFoundException {
+    public UserImpl findUserByName(String name) {
         return findUserWithAttributesByName(name);
     }
 
-    public UserWithAttributes findUserWithAttributesByName(final String name) throws ObjectNotFoundException {
+    public UserImpl findUserWithAttributesByName(final String name) {
         return new UserImpl(this, name);
     }
 
@@ -92,7 +108,20 @@ public class JavaNetDirectory extends AbstractRemoteDirectory {
 
     @Override
     public List searchGroups(EntityQuery entityQuery) {
+        LOGGER.fine("searchGroups: "+entityQuery);
+
         return asList(confluenceAdministrators,confluenceUsers,jiraUsers,jiraAdministrators,jiraDevelopers);
+    }
+
+    /**
+     * JIRA calls this method with {@link NullRestriction} (which presumably means "SELECT *"), and if
+     * we don't return all the users, JIRA will fail with http://jira.atlassian.com/browse/CWD-202
+     * (users are unable to login, can't search with that user, etc.)
+     */
+    @Override
+    public List searchUsers(EntityQuery entityQuery) {
+        LOGGER.fine("searchUsers: "+entityQuery);
+        return new ArrayList<UserImpl>(allUsers.values());
     }
 
     @Override
@@ -102,6 +131,7 @@ public class JavaNetDirectory extends AbstractRemoteDirectory {
         if (groupName.equals(JIRA_ADMINISTRATORS) && userName.equals("abayer"))     return true;
 
         // TODO: how to handle jira-developers category?
+        if (groupName.equals(JIRA_DEVELOPERS) && userName.startsWith("a"))     return true;
 
         return userName.equals("kohsuke");
     }
@@ -117,6 +147,18 @@ public class JavaNetDirectory extends AbstractRemoteDirectory {
             }
             return r;
         }
+
+        if (q.getEntityToMatch().getEntityType()==Entity.GROUP && q.getEntityToReturn().getEntityType()==Entity.USER) {
+            // looking up users by the group
+            List<String> r = new ArrayList<String>();
+            for (String user : allUsers.keySet()) {
+                if (isUserDirectGroupMember(user,q.getEntityNameToMatch()))
+                    r.add(user);
+            }
+            return r;
+        }
+
+        LOGGER.fine("Unhandled searchGroupRelationships: "+q);
         return super.searchGroupRelationships(q);
     }
 
@@ -128,4 +170,14 @@ public class JavaNetDirectory extends AbstractRemoteDirectory {
     private static final String JIRA_ADMINISTRATORS = "jira-administrators";
 
     private static final boolean testMode = Boolean.getBoolean(JavaNetDirectory.class.getName()+".test");
+
+    private static final Logger LOGGER = Logger.getLogger(JavaNetDirectory.class.getName());
+
+    static {
+        LOGGER.setLevel(Level.FINEST);
+        LOGGER.setUseParentHandlers(false);
+        ConsoleHandler h = new ConsoleHandler();
+        h.setLevel(Level.FINEST);
+        LOGGER.addHandler(h);
+    }
 }
