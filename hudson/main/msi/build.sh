5997fde6cf14cb4f09875f78bb26c59e07dae939
rseguy <rseguy@jenkins-ci.org>
2009-10-27 21:12:41 +0000

    [maven-scm] copy for tag copy-to-slave-1.0
    git-svn-id: https://svn.jenkins-ci.org/tags/copy-to-slave-1.0@23247 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/main/msi/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-rm -rf tmp || true
-mkdir tmp || true
-unzip -p hudson.war 'WEB-INF/lib/hudson-core-*.jar' > tmp/core.jar
-unzip -p tmp/core.jar windows-service/hudson.exe > tmp/hudson.exe
-unzip -p tmp/core.jar windows-service/hudson.xml > tmp/hudson.xm_
-# replace executable name to the bundled JRE
-sed -e 's|executable.*|executable>%BASE%\\jre\\bin\\java</executable>|' < tmp/hudson.xm_ > tmp/hudson.xml
