0c13404d1977975d2f0798bc110ea59be3e0b85f
mindless <mindless@jenkins-ci.org>
2010-01-21 20:25:57 +0000

    [maven-scm] copy for tag blame-upstream-commiters-1.1
    git-svn-id: https://svn.jenkins-ci.org/tags/blame-upstream-commiters-1.1@26198 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/extras/firefox-extension/buildmonitor/src/main/js/firefox/loggerservice.js
+++
@@ -1,8 +0,0 @@
-var HudsonLoggerService = Base.extend ({
-	constructor: function(ffConsoleService) {
-		this.ffConsoleService = ffConsoleService;
-	},
-	debug: function(message) {
-		this.ffConsoleService.logStringMessage(message);
-	}
-});
