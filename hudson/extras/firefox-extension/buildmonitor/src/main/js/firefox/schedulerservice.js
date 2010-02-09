80051506b283bc6e8d13b2396b6f349ab7105d13
beryx <beryx@jenkins-ci.org>
2010-02-09 22:30:34 +0000

    [maven-scm] copy for tag groovy-postbuild-1.1
    git-svn-id: https://svn.jenkins-ci.org/tags/groovy-postbuild-1.1@27230 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/extras/firefox-extension/buildmonitor/src/main/js/firefox/schedulerservice.js
+++
@@ -1,8 +0,0 @@
-org_hudsonci.SchedulerService = Base.extend ({
-    constructor: function(ffTimerService) {
-        this.ffTimerService = ffTimerService;
-    },
-    schedule: function(callback, interval) {
-        this.ffTimerService.initWithCallback(callback, interval * 60 * 1000, Components.interfaces.nsITimer.TYPE_ONE_SHOT);
-    }
-});
