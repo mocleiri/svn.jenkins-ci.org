07d122d44cb507d5e76980ca61d7b4dcc18de71d
jglick <jglick@jenkins-ci.org>
2010-02-16 02:21:13 +0000

    [maven-scm] copy for tag toolenv-1.0
    git-svn-id: https://svn.jenkins-ci.org/tags/toolenv-1.0@27544 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/backend/plugin2rpm/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-mvn -e clean install exec:java -Dexec.mainClass=plugin2rpm.App
-find target/RPMS -name '*.rpm' | xargs rpm-sign $(cat ~/.gpg.passphrase)
-cd target
-createrepo .
-rsync -avz RPMS repodata hudson-ci.org:~/www/hudson-ci.org/redhat/plugins
