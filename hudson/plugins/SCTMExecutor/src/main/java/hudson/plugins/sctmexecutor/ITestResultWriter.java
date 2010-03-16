4949ae7d4af4952e32e8071218971d88991cdb2e
drulli <drulli@jenkins-ci.org>
2010-03-16 06:42:18 +0000

    [maven-scm] copy for tag findbugs-4.4
    git-svn-id: https://svn.jenkins-ci.org/tags/findbugs-4.4@28691 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/SCTMExecutor/src/main/java/hudson/plugins/sctmexecutor/ITestResultWriter.java
+++
@@ -1,7 +0,0 @@
-package hudson.plugins.sctmexecutor;
-
-import com.borland.sctm.ws.execution.entities.ExecutionResult;
-
-public interface ITestResultWriter {
-  void write(ExecutionResult result);
-}
