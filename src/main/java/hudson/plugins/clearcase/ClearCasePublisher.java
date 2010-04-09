9bc8a6ff0b35a02b377bba02094d8c827e561fbb
vlatombe <vlatombe@jenkins-ci.org>
2010-04-09 11:24:08 +0000

    License header
    git-svn-id: https://svn.jenkins-ci.org/branches/HUDSON-4569@29906 71c3de6d-444a-0410-be80-ed276b4c234a

--- src/main/java/hudson/plugins/clearcase/ClearCasePublisher.java
+++ src/main/java/hudson/plugins/clearcase/ClearCasePublisher.java
@@ -1,46 +1,42 @@
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2007-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Erik Ramfelt,
+ *                          Henrik Lynggaard, Peter Liljenberg, Andrew Bayer, Vincent Latombe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package hudson.plugins.clearcase;
 
-import hudson.FilePath.FileCallable;
 import hudson.Launcher;
-import hudson.*;
-import hudson.Util;
-import static hudson.Util.fixEmpty;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
 import hudson.model.Descriptor;
-import hudson.model.Hudson;
-import hudson.model.Project;
-import hudson.model.Result;
-import hudson.model.Action;
-import hudson.remoting.RemoteOutputStream;
-import hudson.remoting.VirtualChannel;
-import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Publisher;
-import hudson.util.FormFieldValidator;
-import org.apache.tools.ant.DirectoryScanner;
-import org.apache.tools.ant.types.FileSet;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
-import org.kohsuke.stapler.DataBoundConstructor;
 
-import javax.servlet.ServletException;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.Serializable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
+import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-
-//import hudson.plugins.logparser.util.BuildResult;
-
 
 /**
  * Display ClearCase information report  for build
@@ -133,8 +129,6 @@ public class ClearCasePublisher extends Publisher implements Serializable {
 
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.NONE ;
-	}
-	
-	
+    }
 }
 
