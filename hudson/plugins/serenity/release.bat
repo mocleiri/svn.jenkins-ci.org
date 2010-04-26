860d7bc1c577c0b1107a78a6691e143fdcca3b31
tpayne <tpayne@jenkins-ci.org>
2010-04-26 16:45:01 +0000

    [maven-scm] copy for tag dimensionsscm-0.7.5
    git-svn-id: https://svn.jenkins-ci.org/tags/dimensionsscm-0.7.5@30408 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/serenity/release.bat
+++
@@ -1,7 +0,0 @@
-set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:MaxPermSize=256m
-
-# mvn -DskipTests=true package -DdryRun=true
-# -B -Dresume=false -Dusername=michaelcouck -Dpassword=suineg
-# mvn release:rollback
-
-mvn -DskipTests=true -Dresume=false -DXms512m -DXmx1024m -DXX:MaxPermSize=256m release:prepare release:perform
