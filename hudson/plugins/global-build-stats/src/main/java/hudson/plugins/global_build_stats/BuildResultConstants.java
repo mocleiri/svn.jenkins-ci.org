dcbce01ea1f2580a4d700a97f55c9e6b7a786d64
teilo <teilo@jenkins-ci.org>
2010-06-30 20:45:45 +0000

    [maven-scm] copy for tag m2release-0.5.1
    git-svn-id: https://svn.jenkins-ci.org/tags/m2release-0.5.1@32403 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/global-build-stats/src/main/java/hudson/plugins/global_build_stats/BuildResultConstants.java
+++
@@ -1,9 +0,0 @@
-package hudson.plugins.global_build_stats;
-
-public interface BuildResultConstants {
-	String ABORTED = "aborted";
-	String SUCCESS = "success";
-	String FAILURES = "failures";
-	String UNSTABLES = "unstables";
-	String NOT_BUILD = "not build";
-}
