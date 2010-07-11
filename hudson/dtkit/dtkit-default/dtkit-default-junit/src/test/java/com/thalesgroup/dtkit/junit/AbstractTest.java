5174e3d1727755fafc3cb8a0fef038f1c31a5fa6
gbois <gbois@jenkins-ci.org>
2010-07-11 22:04:46 +0000

    Fixed phpunit xsd and xsl
    git-svn-id: https://svn.jenkins-ci.org/trunk@32729 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/dtkit/dtkit-default/dtkit-default-junit/src/test/java/com/thalesgroup/dtkit/junit/AbstractTest.java
+++ hudson/dtkit/dtkit-default/dtkit-default-junit/src/test/java/com/thalesgroup/dtkit/junit/AbstractTest.java
@@ -1,6 +1,7 @@
 package com.thalesgroup.dtkit.junit;
 
 import com.thalesgroup.dtkit.util.converter.ConvertUtil;
+import com.thalesgroup.dtkit.util.validator.ValidatorError;
 import com.thalesgroup.dtkit.metrics.api.InputMetricXSL;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
@@ -25,14 +26,22 @@ public class AbstractTest {
         File inputXMLFile = new File(this.getClass().getResource(inputXMLPath).toURI());
 
         //The input file must be valid
-        Assert.assertTrue(inputMetricXSL.validateInputFile(inputXMLFile));
+        boolean inputResult = inputMetricXSL.validateInputFile(inputXMLFile);
+        for (ValidatorError validatorError:inputMetricXSL.getInputValidationErrors()){
+            System.out.println(validatorError);
+        }
+        Assert.assertTrue(inputResult);
 
         ConvertUtil.convert(inputMetricXSL.getClass(), inputMetricXSL.getXslName(), inputXMLFile, outputXMLFile);
         Diff myDiff = new Diff(XSLUtil.readXmlAsString(new File(this.getClass().getResource(expectedResultPath).toURI())), XSLUtil.readXmlAsString(outputXMLFile));
         Assert.assertTrue("XSL transformation did not work" + myDiff, myDiff.similar());
 
         //The generated output file must be valid
-        Assert.assertTrue(inputMetricXSL.validateOutputFile(outputXMLFile));
+        boolean outputResult = inputMetricXSL.validateOutputFile(outputXMLFile);
+        for (ValidatorError validatorError:inputMetricXSL.getOutputValidationErrors()){
+            System.out.println(validatorError);
+        }
+        Assert.assertTrue(outputResult);
 
         outputXMLFile.deleteOnExit();
     }
