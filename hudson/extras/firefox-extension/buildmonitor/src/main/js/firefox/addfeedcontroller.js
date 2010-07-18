9c039071b39fb4746dea3b0daab4c4fae41982e3
StevenGBrown <StevenGBrown@jenkins-ci.org>
2010-07-18 08:56:16 +0000

    [maven-scm] copy for tag files-found-trigger-1.1.3
    git-svn-id: https://svn.jenkins-ci.org/tags/files-found-trigger-1.1.3@33013 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/extras/firefox-extension/buildmonitor/src/main/js/firefox/addfeedcontroller.js
+++
@@ -1,8 +0,0 @@
-org_hudsonci.prepare = function() {
-	org_hudsonci_buildMonitorManager.getAddFeedWindow().prepare();
-};
-org_hudsonci.submit = function() {
-	org_hudsonci_buildMonitorManager.getAddFeedWindow().save();
-	var parent = window.arguments[0];
-	parent.org_hudsonci_buildMonitorManager.getBuildMonitor().runAll();
-};
