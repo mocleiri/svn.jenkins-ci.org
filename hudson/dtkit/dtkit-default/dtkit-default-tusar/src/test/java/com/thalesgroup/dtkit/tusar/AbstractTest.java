68731a5de3174e5394c57d72062259bcd7cd31c1
gbois <gbois@jenkins-ci.org>
2010-07-30 08:05:29 +0000

    Updated with the new DI style and the usage of Guice library
    git-svn-id: https://svn.jenkins-ci.org/trunk@33371 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/dtkit/dtkit-default/dtkit-default-tusar/src/test/java/com/thalesgroup/dtkit/tusar/AbstractTest.java
+++ hudson/dtkit/dtkit-default/dtkit-default-tusar/src/test/java/com/thalesgroup/dtkit/tusar/AbstractTest.java
@@ -1,16 +1,36 @@
 package com.thalesgroup.dtkit.tusar;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
 import com.thalesgroup.dtkit.metrics.api.InputMetricXSL;
-import com.thalesgroup.dtkit.util.converter.ConvertUtil;
-import com.thalesgroup.dtkit.util.validator.ValidatorError;
+import com.thalesgroup.dtkit.util.converter.ConversionService;
+import com.thalesgroup.dtkit.util.validator.ValidationError;
+import com.thalesgroup.dtkit.util.validator.ValidationService;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 import java.io.File;
 
 public class AbstractTest {
+
+    private static Injector injector;
+
+    @BeforeClass
+    public static void initInjector() {
+        injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                //Optional binding, provided by default in Guice)
+                bind(ValidationService.class).in(Singleton.class);
+                bind(ConversionService.class).in(Singleton.class);
+            }
+        });
+    }
 
     @Before
     public void setUp() {
@@ -20,21 +40,23 @@ public class AbstractTest {
     }
 
 
-    public void convertAndValidate(Class<? extends InputMetricXSL> classType, String inputXMLPath, String expectedResultPath) throws Exception {
-        InputMetricXSL inputMetricXSL = classType.newInstance();
+    public void convertAndValidate(Class<? extends InputMetricXSL> inputMetricXSLClassType, String inputXMLPath, String expectedResultPath) throws Exception {
+
+        InputMetricXSL inputMetricXSL = injector.getInstance(inputMetricXSLClassType);        
+
         File outputXMLFile = File.createTempFile("result", "xml");
         File inputXMLFile = new File(this.getClass().getResource(inputXMLPath).toURI());
 
         //The input file must be valid
         Assert.assertTrue(inputMetricXSL.validateInputFile(inputXMLFile));
 
-        ConvertUtil.convert(inputMetricXSL.getClass(), inputMetricXSL.getXslName(), inputXMLFile, outputXMLFile);
+        inputMetricXSL.convert(inputXMLFile, outputXMLFile);
         Diff myDiff = new Diff(XSLUtil.readXmlAsString(new File(this.getClass().getResource(expectedResultPath).toURI())), XSLUtil.readXmlAsString(outputXMLFile));
         Assert.assertTrue("XSL transformation did not work" + myDiff, myDiff.similar());
 
         //The generated output file must be valid
         boolean result = inputMetricXSL.validateOutputFile(outputXMLFile);
-        for (ValidatorError validatorError : inputMetricXSL.getOutputValidationErrors()) {
+        for (ValidationError validatorError : inputMetricXSL.getOutputValidationErrors()) {
             System.out.println("[ERROR] " + validatorError.toString());
         }
         Assert.assertTrue(result);
