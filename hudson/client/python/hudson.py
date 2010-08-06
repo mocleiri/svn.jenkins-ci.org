bf3a5e861ab4246c716e3c5aea50a234d5ee1415
mcrooney <mcrooney@jenkins-ci.org>
2010-08-06 19:28:24 +0000

    add initial hudson.py hudson python client
    git-svn-id: https://svn.jenkins-ci.org/trunk@33603 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ hudson/client/python/hudson.py
@@ -0,0 +1,75 @@
+"""
+A Python interface to a Hudson instance.
+
+Please contribute!
+ * submit bugs/patches/requests via issues.hudson-ci.org
+ * email mcrooney@dev.java.net with bugs/patches/requests
+
+TODO:
+ * remove dep on BeautifulSoup in getTestFailuresForJob, use /api/python instead
+ * eliminate wget shell call in createJob via native urllib[2]
+"""
+import re
+import urllib2
+
+def sanitize(arg):
+    """Sanitize an argument to put in a shell command."""
+    return arg.replace("\"", "\\\"").replace("$", r"\$").replace("`", r"\`")
+
+class HudsonInstance:
+    def __init__(self, hudsonUrl):
+        self.URL = hudsonUrl
+
+    def read(self, url):
+        fullUrl = self.URL + url
+        return urllib2.urlopen(fullUrl).read()
+
+    def build(self, job, parameters="", token=None):
+        url = "/job/%s/build" % job
+        if parameters:
+            url += "WithParameters?%s" % parameters
+            if token:
+                url += "&token=%s" % token
+        elif token:
+            url += "?token=%s" % token
+        self.read(url)
+
+    def getLastBuildNumberForJob(self, job):
+        contents = self.read("/job/%s/lastBuild/" % job)
+        last = re.search("Build \#(\d+)", contents).groups()[0]
+        last = int(last)
+        return last
+
+    def getTestFailuresForJob(self, job, buildNumber=None):
+        from BeautifulSoup import BeautifulStoneSoup
+        if buildNumber is None:
+            buildNumber = self.getLastBuildNumberForJob(job)
+
+        try:
+            testResultXml = self.read("/job/%s/%s/testReport/api/xml" % (job, buildNumber))
+        except urllib2.HTTPError:
+            return None
+
+        soup = BeautifulStoneSoup(testResultXml)
+        failures = []
+        for test in soup.findAll("case"):
+            if test.status.contents[0] not in ["PASSED", "FIXED"]:
+                test.AGE = int(test.age.contents[0])
+                test.NAME = test.find("name").contents[0]
+                failures.append(test)
+        return failures
+
+    def getConfigForJob(self, job):
+        """Read and return the config.xml of the specified job."""
+        return self.read("/job/%s/config.xml" % job)
+
+    def createJob(job, config, build=False, buildToken=None):
+        """Given a name and a config.xml, create a new Hudson job, optionally triggering a build."""
+        url = self.URL + "/createItem?name=%s" % job
+        cmd = 'wget --header="Content-Type: text/xml" --post-data="%s" -O - "%s"' % (sanitize(config), url)
+        status, output = commands.getstatusoutput(cmd)
+        if status or "200 OK" not in output:
+            raise Exception("wget failed for %s: %s" % (job, output))
+        if build:
+            self.build(job, token=buildToken)
+
