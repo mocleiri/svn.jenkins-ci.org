e3c2d8ee920c7b798b20fa283c1c80a57a8e0d62
ragesh_nair <ragesh_nair@jenkins-ci.org>
2010-09-15 00:36:15 +0000

    [maven-scm] copy for tag rebuild-1.4
    git-svn-id: https://svn.jenkins-ci.org/tags/rebuild-1.4@34771 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/global-build-stats/src/main/java/hudson/plugins/global_build_stats/xstream/migration/GlobalBuildStatsPOJO.java
+++
@@ -1,8 +0,0 @@
-package hudson.plugins.global_build_stats.xstream.migration;
-
-/**
- * Generic interface for GlobalBuildStats POJOs
- * @author fcamblor
- */
-public interface GlobalBuildStatsPOJO {
-}
