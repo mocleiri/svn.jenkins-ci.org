a3c5c37051d4740f812f552f2c28276af4b8480e
teilo <teilo@jenkins-ci.org>
2010-09-16 21:57:59 +0000

    [maven-scm] copy for tag m2release-0.6.0
    git-svn-id: https://svn.jenkins-ci.org/tags/m2release-0.6.0@34941 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/global-build-stats/src/main/java/hudson/plugins/global_build_stats/model/BuildHistorySearchCriteria.java
+++
@@ -1,9 +0,0 @@
-package hudson.plugins.global_build_stats.model;
-
-public class BuildHistorySearchCriteria {
-
-	public String jobFilter;
-	public long start, end;
-	public boolean successShown, failuresShown, unstablesShown, abortedShown, notBuildShown;
-	
-}
