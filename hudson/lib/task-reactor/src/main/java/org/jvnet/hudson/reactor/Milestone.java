e684108838a29dba90ac376247ae1918700d9637
fcamblor <fcamblor@jenkins-ci.org>
2010-09-16 14:13:59 +0000

    [maven-scm] copy for tag global-build-stats-0.4.1
    git-svn-id: https://svn.jenkins-ci.org/tags/global-build-stats-0.4.1@34928 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/lib/task-reactor/src/main/java/org/jvnet/hudson/reactor/Milestone.java
+++
@@ -1,9 +0,0 @@
-package org.jvnet.hudson.reactor;
-
-/**
- * Marker interface that indicates a milestone in the reactor process.
- *
- * @author Kohsuke Kawaguchi
- */
-public interface Milestone {
-}
