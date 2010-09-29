525d922e52a53d45b2b0f83bd44621b30c395fef
rpetti <rpetti@jenkins-ci.org>
2010-09-29 18:32:08 +0000

    [maven-scm] copy for tag perforce-1.1.9
    git-svn-id: https://svn.jenkins-ci.org/tags/perforce-1.1.9@35375 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/extras/firefox-extension/buildmonitor/src/main/js/core/feednotifier.js
+++
@@ -1,6 +0,0 @@
-org_hudsonci.FeedNotifier = name_edwards_dean_Base.extend({
-	constructor: function(notification, preferences) {
-		this.notification = notification;
-		this.preferences = preferences;
-	}
-});
