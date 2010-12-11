6b9daee7e60a93b4ee9323402bf39902cb15fd42
kohsuke <kohsuke@jenkins-ci.org>
2010-12-11 19:39:49 +0000

    manufacturing proper tag for maven-hpi-plugin-1_13/
    git-svn-id: https://svn.jenkins-ci.org/tags/maven-hpi-plugin-1.13@37364 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/main/remoting/src/main/java/hudson/remoting/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Remoting infrastructure for Hudson.
- *
- * <p>
- * Code in this package is used for running a part of a program in slaves.
- * If you are new to this package, start from {@link Channel}.
- */
-package hudson.remoting;
