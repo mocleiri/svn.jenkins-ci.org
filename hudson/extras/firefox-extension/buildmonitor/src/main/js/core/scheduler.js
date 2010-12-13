81e7dc318d2f4edec42ddeb68403d2841827a480
kinow <kinow@jenkins-ci.org>
2010-12-13 16:52:38 +0000

    [maven-scm] copy for tag testlink-2.0.1
    git-svn-id: https://svn.jenkins-ci.org/tags/testlink-2.0.1@37409 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/extras/firefox-extension/buildmonitor/src/main/js/core/scheduler.js
+++
@@ -1,8 +0,0 @@
-org_hudsonci.Scheduler = name_edwards_dean_Base.extend({
-    constructor: function(service) {
-        this.service = service;
-    },
-    schedule: function(callback, interval) {
-        this.service.schedule(callback, interval);
-    }
-});
