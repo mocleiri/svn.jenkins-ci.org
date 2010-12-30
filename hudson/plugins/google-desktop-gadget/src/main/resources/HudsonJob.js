5a4be4bf61ee7f26afe36fd729296ad73a668bf3
olamy <olamy@jenkins-ci.org>
2010-12-30 21:13:26 +0000

    [maven-release-plugin]  copy for tag hudson-maven-embedder-3.1
    git-svn-id: https://svn.jenkins-ci.org/tags/hudson-maven-embedder-3.1@37700 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/google-desktop-gadget/src/main/resources/HudsonJob.js
+++
@@ -1,9 +0,0 @@
-﻿function HudsonJob() {
-
-	HudsonJob.statusColors = ['red', 'red_anime', 'blue', 'blue_anime', 'yellow', 'yellow_anime', 'grey', 'grey_anime', 'aborted', 'disabled'];
-
-	this.name;
-	this.color;
-	this.url;
-
-}
