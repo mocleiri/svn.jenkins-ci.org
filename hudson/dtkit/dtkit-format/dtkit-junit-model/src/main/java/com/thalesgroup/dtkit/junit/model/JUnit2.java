533da33e8712ea48d333f966d2d0193acdd66e9d
gbois <gbois@jenkins-ci.org>
2011-01-26 22:13:53 +0000

    version 2 of the junit format
    git-svn-id: https://svn.jenkins-ci.org/trunk@38357 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ hudson/dtkit/dtkit-format/dtkit-junit-model/src/main/java/com/thalesgroup/dtkit/junit/model/JUnit2.java
@@ -0,0 +1,36 @@
+package com.thalesgroup.dtkit.junit.model;
+
+import com.thalesgroup.dtkit.metrics.model.AbstractOutputMetric;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+import java.io.Serializable;
+
+@XmlAccessorType(XmlAccessType.PROPERTY)
+public class JUnit2 extends AbstractOutputMetric implements Serializable {
+
+    @Override
+    @XmlElement
+    public String getKey() {
+        return "junit";
+    }
+
+    @Override
+    @XmlElement
+    public String getDescription() {
+        return "JUNIT OUTPUT FORMAT 2.0";
+    }
+
+    @Override
+    @XmlElement
+    public String getVersion() {
+        return "2.0";
+    }
+
+    @Override
+    @XmlElement
+    public String[] getXsdNameList() {
+        return new String[]{"xsd/junit-2.xsd"};
+    }
+}
