4d1d43346ffae3a1447cc69195d4e6ff83562445
gbois <gbois@jenkins-ci.org>
2011-02-08 22:24:34 +0000

    prepare release and reformat code before release
    git-svn-id: https://svn.jenkins-ci.org/trunk@38593 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/dtkit/dtkit-format/dtkit-junit-model/src/main/java/com/thalesgroup/dtkit/junit/model/JUnit2.java
+++ hudson/dtkit/dtkit-format/dtkit-junit-model/src/main/java/com/thalesgroup/dtkit/junit/model/JUnit2.java
@@ -2,9 +2,9 @@ package com.thalesgroup.dtkit.junit.model;
 
 import com.thalesgroup.dtkit.metrics.model.AbstractOutputMetric;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import java.io.Serializable;
 
 @XmlAccessorType(XmlAccessType.PROPERTY)
