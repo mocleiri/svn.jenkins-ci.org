839664e99d27d9c1b5c96701d332ccd1ec8e7fe8
gbois <gbois@jenkins-ci.org>
2011-08-10 20:51:33 +0000

    Remove unused directory
    git-svn-id: https://svn.jenkins-ci.org/trunk@39755 71c3de6d-444a-0410-be80-ed276b4c234a

---
+++ hudson/dtkit/dtkit-default/tusar-output/dtkit-default-tusar/src/main/java/com/thalesgroup/dtkit/tusar/SourceMonitor.java
@@ -0,0 +1,52 @@
+package com.thalesgroup.dtkit.tusar;
+
+import com.thalesgroup.dtkit.metrics.model.InputMetricXSL;
+import com.thalesgroup.dtkit.metrics.model.InputType;
+import com.thalesgroup.dtkit.metrics.model.OutputMetric;
+import com.thalesgroup.dtkit.processor.InputMetric;
+import com.thalesgroup.dtkit.tusar.model.TusarModel;
+
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author Gregory Boissinot
+ */
+@XmlType(name = "sourceMonitor", namespace = "tusar")
+@InputMetric
+public class SourceMonitor extends InputMetricXSL {
+
+    @Override
+    public InputType getToolType() {
+        return InputType.TEST;
+    }
+
+    @Override
+    public String getToolName() {
+        return "SourceMonitor";
+    }
+
+    @Override
+    public String getToolVersion() {
+        return "2.6";
+    }
+
+    @Override
+    public boolean isDefault() {
+        return true;
+    }
+
+    @Override
+    public String getXslName() {
+        return "sourcemonitor-2.6-1.0-to-tusar-8.0.xsl";
+    }
+
+    @Override
+    public String[] getInputXsdNameList() {
+        return null;
+    }
+
+    @Override
+    public OutputMetric getOutputFormatType() {
+        return TusarModel.OUTPUT_TUSAR_8_0;
+    }
+}
