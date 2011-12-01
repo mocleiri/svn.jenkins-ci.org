8598e06cb704dc3772045347f7789e48128aab35
kohsuke <kohsuke@jenkins-ci.org>
2011-12-01 00:43:10 +0000

    Added a target to invoke FindBugs.
    git-svn-id: https://svn.jenkins-ci.org/trunk@40157 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/test-projects/model-ant-project/src/Hello.java
+++ hudson/test-projects/model-ant-project/src/Hello.java
@@ -1,5 +1,11 @@
-public class Hello {
+public final class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+    }
+
+    // dummy method to make FindBugs report some problems
+    private void unusedMethod() {
+        String x = null;
+        System.out.println("foo".toString()+(x.toString()));
     }
 }
