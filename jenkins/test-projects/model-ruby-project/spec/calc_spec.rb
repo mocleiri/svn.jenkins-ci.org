a0de0ac338f18822d0d47edadaf4f35b12f2db65
kohsuke <kohsuke@jenkins-ci.org>
2012-01-04 09:01:46 +0000

    split to 2
    git-svn-id: https://svn.jenkins-ci.org/trunk@40226 71c3de6d-444a-0410-be80-ed276b4c234a

--- jenkins/test-projects/model-ruby-project/spec/calc_spec.rb
+++ jenkins/test-projects/model-ruby-project/spec/calc_spec.rb
@@ -5,7 +5,9 @@ describe Calculator, "#add" do
   it "returns 2 for adding 1+1" do
     Calculator.new.add(1,1).should == 2
   end
+end
 
+describe Calculator, "#sub" do
   it "returns 0 for subtracting 1-1" do
     Calculator.new.sub(1,1).should == 0
   end
