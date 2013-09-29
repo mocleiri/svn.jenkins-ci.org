b400001a1e078cc019927f2ae97a7e258cffa9c5
evernat <evernat@jenkins-ci.org>
2013-09-29 12:39:07 +0000

    [maven-scm] copy for tag monitoring-1.47.0
    git-svn-id: https://svn.jenkins-ci.org/tags/monitoring-1.47.0@40999 71c3de6d-444a-0410-be80-ed276b4c234a

--- hudson/plugins/drools/drools/src/main/java/hudson/drools/SessionCallable.java
+++
@@ -1,9 +0,0 @@
-package hudson.drools;
-
-import org.drools.runtime.StatefulKnowledgeSession;
-
-public interface SessionCallable<T> {
-
-	T call(StatefulKnowledgeSession session) throws Exception;
-	
-}
